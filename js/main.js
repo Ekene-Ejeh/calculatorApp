8312cd1548d38d8bf31c74dcaa6967fde31f9a7f
Ekene Ejeh <ekeneejeh@gmail.com>
2021-10-17 11:08:48 -0700

    done with the styling of the calculator

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".btn");
+const screen = document.querySelector(".screen");
+const equalBtn = document.querySelector(".btn-equal");
+const clearBtn = document.querySelector(".btn-clear");
+
+for (i = 0; i < btn.length; i++) {}
