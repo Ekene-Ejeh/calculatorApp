75e3ede382a4d796b09710aad6c077972ae6587b
Ekene Ejeh <ekeneejeh@gmail.com>
2021-10-17 13:05:19 -0700

    functionality of the calculator

--- js/main.js
+++ js/main.js
@@ -3,4 +3,22 @@ const screen = document.querySelector(".screen");
 const equalBtn = document.querySelector(".btn-equal");
 const clearBtn = document.querySelector(".btn-clear");
 
-for (i = 0; i < btn.length; i++) {}
+for (let i = 0; i < btn.length; i++) {
+	btn[i].addEventListener("click", () => {
+		let number = btn[i].getAttribute("data-num");
+		screen.value += number;
+	});
+}
+
+equalBtn.addEventListener("click", () => {
+	if (screen.value === "") {
+		alert("No number selected!");
+	} else {
+		let value = eval(screen.value);
+		screen.value = value;
+	}
+});
+
+clearBtn.addEventListener("click", () => {
+	screen.value = "";
+});
